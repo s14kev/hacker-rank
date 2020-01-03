607a79a24ed1b293afa130deec16b0cea376a045
Kevin Lee <klee3@apple.com>
2020-01-03 11:34:46 -0800

    Solved.

--- find-digits.py
+++ find-digits.py
@@ -1,8 +1,18 @@
 def findDigits(n):
     #convert to string then convert digits back to int (list)
+    stringify = str(n)
+    digitlist = []
+    divisorsCounter = 0
+    for element in stringify:
+        if element != '0':
+            digitlist.append(int(element))
     #check if each list element is divisor
     #if it is, add 1 to the number of divisors counter
+    for digit in digitlist:
+        if n % digit == 0:
+            divisorsCounter = divisorsCounter + 1
     #return number of divisors counter
+    return(divisorsCounter)
 
 t = int(input())
 
